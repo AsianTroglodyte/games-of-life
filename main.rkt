d990cb57862125fdc3152700664efd0a62f3ac6a
Karan Swansi <kswansi2002@gmail.com>
2026-07-15 19:32:42 -0400

    added more GUI stuff (experimentation)

--- main.rkt
+++ main.rkt
@@ -33,32 +33,35 @@
   (let ([grid-col (create-col n picture)])
     (create-row n grid-col)))
 
+
 ;; starting up the GUI
 (define window (new frame%
                     [label "Conway's Game of Life"]
                     [width 300]
-                    [height 300]))
+                    [height 300]
+                    [alignment '(center center)]))
 
-(define yellow-circle
-  (colorize (circle 20) "yellow"))
+(define msg (new message%
+                 [parent window]
+                 [label "New message"]))
+
+(new button%
+     [parent window]
+     [label "Click Me"]
+     ; Callback procedure for a button click:
+     [callback (lambda (button event)
+                 (send msg set-label "Button click"))])
 
 (define (add-drawer picture)
   (let ([drawer (make-pict-drawer picture)])
     (new canvas%
          [parent window]
          [style '(border)]
+         ;; [alignment '(center center)]
          [paint-callback (lambda (self dc)
                            (drawer dc 0 0))])))
 
 (add-drawer (square-grid 5 black-square))
-(square-grid 5 black-square)
 
-;; (white-square)
-;;      [parent window]
-;;      [style 'border]
-;;      [paint-callback ])
-
-;; (define (add-drawing p)
-;; (let )
 
 (send window show #t)
