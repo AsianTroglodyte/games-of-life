7c73c7d1dc3492d3add466193f52457150461e88
Karan Swansi <kswansi2002@gmail.com>
2026-07-15 23:39:52 -0400

    basic mouse events and positions captured

--- main.rkt
+++ main.rkt
@@ -6,32 +6,6 @@
 
 (require "game_state.rkt")
 
-;; (define (colored-square side-size color)
-;;   (colorize (square side-size) color))
-
-;; (define black-square (colored-square 10 "black"))
-
-;; (define (create-col col-height pict)
-;;   (if (> col-height 1)
-;;       (vc-append 2 pict
-;;                  (create-col
-;;                   (- col-height 1)
-;;                   (vc-append 2 pict)))
-;;       pict))
-
-;; (define (create-row row-width pict)
-;;   (if (> row-width 1)
-;;       (hc-append 2 pict
-;;                  (create-row
-;;                   (- row-width 1)
-;;                   (hc-append 2 pict)))
-;;       pict))
-
-;; (define (square-grid n picture)
-;;   (let ([grid-col (create-col n picture)])
-;;     (create-row n grid-col)))
-
-
 ;; starting up the GUI
 (define window (new frame%
                     [label "Conway's Game of Life"]
@@ -41,15 +15,6 @@
 
 (define main (new vertical-panel%
                   [parent window]))
-
-;; (define (add-drawer picture)
-;;   (let ([drawer (make-pict-drawer picture)])
-;;     (new canvas%
-;;          [parent main]
-;;          [style '(border)]
-;;          ;; [alignment '(center center)]
-;;          [paint-callback (lambda (self dc)
-;;                            (drawer dc 0 0))])))
 
 (define msg (new message%
                  [parent main]
@@ -63,16 +28,11 @@
       (lambda (button event)
         (send msg set-label "Button click"))])
 
-;; (add-drawer (square-grid 5 black-square))
-;; (for-each displayln grid-4)
 
 (define grid-size-cells 10)   ; plural because the unit is "cells"
 (define cell-size-px 10)
 (define grid-size-px
   (* grid-size-cells cell-size-px))
-
-
-(define grid (create-grid grid-size-cells))
 
 (define game-canvas
   (make-bitmap grid-size-px grid-size-px))
@@ -88,8 +48,8 @@
          x-pos
          y-pos)
   (send dc draw-rectangle
-      x-pos y-pos
-      cell-size-px cell-size-px))
+      x-pos         y-pos
+      cell-size-px  cell-size-px))
 
 (define test-grid (list
                (list 0 0 1)
@@ -109,9 +69,18 @@
 
 (draw-grid test-grid dc)
 
-game-canvas
+;; game-canvas
+(define game-canvas%
+  (class canvas%
+    (super-new)
+    (define/override (on-event evt)
+      (when (send evt button-down?)
+        (send msg set-label
+              (format "Clicked at ~a, ~a"
+                      (send evt get-x)
+                      (send evt get-y)))))))
 
-(new canvas%
+(new game-canvas%
      [parent main]
      [style '(border)]
      [paint-callback
@@ -119,4 +88,5 @@ game-canvas
         (send dc set-scale 3 3)
         (send dc set-text-foreground "white")
         (send dc draw-bitmap game-canvas 0 0))])
-;; (send window show #t)
+
+(send window show #t)
